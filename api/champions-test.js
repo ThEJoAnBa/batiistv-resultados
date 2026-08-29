b5c7d13161f5b4d5685500b63ccb5b7a918f0640
ThEJoAnBa <157521992+ThEJoAnBa@users.noreply.github.com>
2026-08-29 18:13:43 +0200

    Update champions-test.js

--- api/champions-test.js
+++ api/champions-test.js
@@ -1,6 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    mensaje: "champions-test funciona"
-  });
+export default async function handler(req, res) {
+  try {
+    return res.status(200).json({
+      ok: true,
+      mensaje: "champions-test funciona"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
 }
